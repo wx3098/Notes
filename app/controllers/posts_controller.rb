64ed6caa34f3c14e659442ea91001f47f5b54287
kouichi shinkokura <stndard809@gmail.com>
2022-12-05 22:31:38 +0900

    edit,update,ダイヤログ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+  
   def index
     @posts = Post.all 
   end
@@ -8,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(bord_params)
+    @post = Post.new(note_params)
      if @post.save
      redirect_to new_post_path,notice: "メモを作成しました"
     else
@@ -17,16 +19,32 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
+
+  def update
+    if @post.update(note_params)
+      redirect_to posts_path, notice: "Noteを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Noteを削除しました"
+  end
+
 
   private
 
-  def bord_params
+  def note_params
     params.require(:post).permit(:content)
+  end
+
+  def set_note
+    @post = Post.find(params[:id])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,7 @@ Bundler.require(*Rails.groups)
 module Board
   class Application < Rails::Application
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
-
-  config.time_zone = 'Tokyo'
-  config.active_record.:default_timezone = :local
 end
